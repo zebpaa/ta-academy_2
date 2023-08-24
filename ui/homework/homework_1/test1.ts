6c6547deac058125c00dc69a4d9411e8f18dce9b
alexandrashmoylova <shmoylova.a@optimax.dev>
2023-08-24 18:09:10 +0400

    add homework to lesson_1

---
+++ ui/homework/homework_1/test1.ts
@@ -0,0 +1,33 @@
+/*
+ - Remove all `any` type
+ - Fix type `person`
+*/
+
+type Person = {
+  occupation?: string;
+};
+
+const users: Person[] = [
+  {
+    name: 'Owen Willson',
+    age: 54,
+    occupation: 'Actor',
+  },
+  {
+    name: 'Ben Stiller',
+    age: 57,
+    occupation: 'Filmmaker',
+  },
+  {
+    name: 'Batman',
+    age: 'unknown',
+    isSuperHero: true,
+    occupation: 'vigilante',
+  },
+];
+
+function logPerson(user: Person) {
+  console.log(` - ${user.name}, ${user.age}`);
+}
+
+users.forEach(logPerson);
