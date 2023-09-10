4442fbc2d9836030fd4a53450a09f12ce499f025
Aleksei Skvortsov <skvorcov_90@mail.ru>
2023-09-10 13:47:03 +0400

    completed the 5th task

--- integration/__specs__/tests/analytics/homework_5.spec.ts
+++ integration/__specs__/tests/analytics/homework_5.spec.ts
@@ -1,7 +1,7 @@
 import { CartPage } from '@Components/cartPage/cartPage';
 import { Mock } from '@Core/mock';
 import { GetCartItemsMock } from '@Mocks/api/mockio/v1/id/get';
-import { findAllInDataLayer, waitForDataLayer } from '@Utils/dataLayer';
+import { waitForDataLayer } from '@Utils/dataLayer';
 import { faker } from '@faker-js/faker';
 
 describe('Check dataLayer event on cart page', () => {
@@ -9,9 +9,9 @@ describe('Check dataLayer event on cart page', () => {
     let cartPage: CartPage;
     // Init random data
     const random = {
-        name: 'Bike',
-        price: 200,
-        quantity: 3,
+        name: faker.commerce.product(),
+        price: faker.number.int({ min: 10, max: 100 }),
+        quantity: faker.number.int({ min: 1, max: 10 }),
     };
 
     beforeAll(() => {
@@ -41,50 +41,52 @@ describe('Check dataLayer event on cart page', () => {
         const quantityInput = await cartModal.getCartQuantityInput();
 
         // Filling fields
-        await nameInput.input(random.name);
-        await priceInput.input(random.price.toString());
-        await quantityInput.input(random.quantity.toString());
+        nameInput.input(random.name);
+        priceInput.input(random.price.toString());
+        quantityInput.input(random.quantity.toString());
 
         await cartModal.createCartItemButton(); // Click on 'Create'
 
         reporter.startStep('Add item event');
         const addItemEvent = await waitForDataLayer({ name: `Add item - ${random.name}` });
         expect(addItemEvent).toStrictEqual({
-            name: `Add item - ${name}`,
+            name: `Add item - ${random.name}`,
             price: random.price,
             quantity: random.quantity,
             value: random.name,
         });
         reporter.endStep();
 
-        // const list = await cartPage.getCartList();
-        // const items = await list.getCartItems(); // Get array of items
+        const list = await cartPage.getCartList();
 
-        // // list.debug(); // To check the added item
+        // First item
+        const [item] = await list.getCartItems();
+        await item.deleteItem();
 
-        // // First item
-        // const [item] = await list.getCartItems();
-        // await item.deleteItem();
+        reporter.startStep('Delete item event');
+        const deleteItemEvent = await waitForDataLayer({ name: `Delete item - ${random.name}` });
+        expect(deleteItemEvent).toStrictEqual({
+            name: `Delete item - ${random.name}`,
+            value: random.name,
+        });
+        reporter.endStep();
 
-        // reporter.startStep('Delete item - "your item name" event');
-        // const deleteItemEvent = await waitForDataLayer({ name: "Delete item - 'your item name'" });
-        // expect(deleteItemEvent).toStrictEqual({
-        //     name: "Delete item - 'your item name'",
-        //     value: "your item name"
-        // });
-        // reporter.endStep();
+        // Сycle of deleted remaining elements
+        for (let i = 0; i < 3; i++) {
+            const newList = await cartPage.getCartList();
+            const [newItems] = await newList.getCartItems();
+            await newItems.deleteItem();
+        }
 
-        // // Need to get a new list of elements to verify that the element was removed
-        // const newList = await cartPage.getCartList();
-        // const newItems = await newList.getCartItems();
+        // const emptyList = await cartPage.getCartList();
+        // emptyList.debug(); // To check all deleted items
 
-        // // newList.debug(); // To check the removed item
-
-        // // Function, that delete all items. Need to write!
-
-        // reporter.startStep('Cart is Empty event');
-        // const deleteAllItemsEvent = findAllInDataLayer('Cart is Empty');
-        // expect(deleteAllItemsEvent).toHaveLength(2);
-        // reporter.endStep();
+        reporter.startStep('Cart is Empty event');
+        const deleteAllItemsEvent = await waitForDataLayer({ name: 'Cart is Empty' });
+        expect(deleteAllItemsEvent).toStrictEqual({
+            name: 'Cart is Empty',
+            value: 'Quantity of products: 0',
+        });
+        reporter.endStep();
     });
 });
